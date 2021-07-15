acd8af0d98b62c8393030487e031548c2b8e14fd
JorisMarien <61184778+JorisMarien@users.noreply.github.com>
2021-07-15 11:14:44 +0200

    intercept

--- Project1/cypress/integration/api.js
+++ Project1/cypress/integration/api.js
@@ -2,34 +2,42 @@
 
     it('POST',function(){
         const item = {
-            "id": 150,
-            "category": {
-              "id": 1,
-              "name": "Cats"
-            },
-            "name": "Jos",
-            "photoUrls": [
-              "string"
-            ],
-            "tags": [
-              {
-                "id": 0,
-                "name": "string"
-              }
-            ],
-            "status": "available"
-          }
-        cy.request('POST', 'https://petstore3.swagger.io/api/v3/pet', item).then((response) => {
+          "id": 150,
+          "category": {
+            "id": 5,
+            "name": "Cats"
+          },
+          "name": "Jos",
+          "photoUrls": [
+            "string"
+          ],
+          "tags": [
+            {
+              "id": 0,
+              "name": "string"
+            }
+          ],
+          "status": "available"
+        }
+        cy.request('POST', 'https://petstore.swagger.io/v2/pet', item).then((response) => {
         expect(response).to.have.property('status',200)
         })
     })
 
-    it('GET',function(){
-        cy.request('https://petstore3.swagger.io/api/v3/pet/150').then((response) => {
-            expect(response).to.have.property('status',200)
-            expect(response.body).to.not.be.null
-            expect(response.body).to.have.property('id',150)
+    it.only('GET',function(){
+      cy.intercept('GET','https://petstore.swagger.io/v2/pet/150', (req) =>{
+        req.reply({
+          statusCode:200,
+          fixture:'\intercept\\interceptFixture.json'
         })
+      })
+      
+      cy.request('https://petstore.swagger.io/v2/pet/150').then((response) => {
+        expect(response).to.have.property('status',200)
+        expect(response.body).to.not.be.null
+        expect(response.body).to.have.property('id',150)
+    })    
+      
     })
 
     it('PUT', function(){
